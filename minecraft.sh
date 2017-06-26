96e033f238bb401eddf05b01dba4db81532edf5a
Erik Kellener <erik@Eriks-iMac.local>
2017-06-26 12:47:32 -0700

    Upgraded to 1.12, inc java 8, enabled MC_OP flag for operator

--- minecraft.sh
+++ minecraft.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+#env
+env
+if [ -z ${MC_OPS} ]; then
+ echo "No Ops defined" ;
+ else
+# Lookup UUID
+MC_UUID=`./lookupuuid.sh $MC_OPS`
 
-#if ! screen -list | grep -q "minecraft"; then
-  #cd /home/pi/minecraft
-  #screen  -S minecraft -m  java -jar  -Xms512M -Xmx1008M spigot-1.11.2.jar nogui && screen -r minecraft
-exec  java -jar  -Xms512M -Xmx1008M spigot-1.11.2.jar nogui 
+#format ops.json string
+opstr="
+[
+  {
+    \"uuid\": \"$MC_UUID\",
+    \"name\": \"$MC_OPS\",
+    \"level\": 4,
+    \"bypassesPlayerLimit\": false
+  }
+]"
+
+ echo  $opstr >> /data/ops.json;
+ fi
+java  -Dcom.mojang.eula.agree=true -jar -Xms256M -Xmx512M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+AggressiveOpts spigot-1.12.jar nogui --world-dir /data    --config /data/config/server.properties   --bukkit-settings /data/config/bukkit.yml  --commands-settings /data/config/commands.yml  --spigot-settings /data/config/spigot.yml 
+#exec  java -jar  -Xms512M -Xmx1008M spigot-1.12.jar nogui 
 #fi
